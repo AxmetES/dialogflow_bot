297cee224244b374eeb0880d2938d7818e0327c8
axmetES <fraktsia@gmail.com>
2020-02-27 12:28:00 +0600

    Reade new phrases from file. -Add func to reade new phrases and create new intents. end

--- training.py
+++ training.py
@@ -1,6 +1,7 @@
 import os
-
-from google.api_core.exceptions import InvalidArgument
+import json
+import dialogflow_v2 as dialogflow
+from google.api_core.exceptions import InvalidArgument, FailedPrecondition
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -8,8 +9,6 @@ load_dotenv()
 
 def create_intent(project_id, display_name, training_phrases_parts,
                   message_texts):
-    """Create an intent of the given intent type."""
-    import dialogflow_v2 as dialogflow
     intents_client = dialogflow.IntentsClient()
 
     parent = intents_client.project_agent_path(project_id)
@@ -36,32 +35,27 @@ def create_intent(project_id, display_name, training_phrases_parts,
 
 
 def train_agent(project_id):
-    # , retry = google.api_core.gapic_v1.method.DEFAULT, timeout = google.api_core.gapic_v1.method.DEFAULT,
-    # metadata = None
     import dialogflow_v2beta1
     client = dialogflow_v2beta1.AgentsClient()
     parent = client.project_path(project_id)
-    response = client.train_agent(parent)
-    print(response)
+    client.train_agent(parent)
 
 
 def main():
     project_id = os.getenv('PROJECT_ID')
-    display_name = "Устройство на работу"
-    training_phrases_parts = [
-        "Как устроиться к вам на работу?",
-        "Как устроиться к вам?",
-        "Как работать у вас?",
-        "Хочу работать у вас",
-        "Возможно-ли устроиться к вам?",
-        "Можно-ли мне поработать у вас?",
-        "Хочу работать редактором у вас"
-    ]
-    message_texts = {'''Если вы хотите устроиться к нам, напишите на почту game-of-verbs@gmail.com 
-    мини-эссе о себе и прикрепите ваше портфолио.'''}
 
-    create_intent(project_id, display_name, training_phrases_parts,
-                  message_texts)
+    with open('questions.json', 'r') as file:
+        intents = json.load(file)
+
+    for intent in intents:
+        display_name = intent
+        intent_body = intents[f'{display_name}']
+        training_phrases_parts = intent_body['questions']
+        message_texts = {intent_body['answer']}
+
+        create_intent(project_id, display_name, training_phrases_parts,
+                      message_texts)
+
     train_agent(project_id)
 
 
