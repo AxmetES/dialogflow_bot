c5153a06b5acc0bcde9fbfb34c95067a6f96bef0
axmetES <fraktsia@gmail.com>
2020-03-16 23:29:53 +0600

    -Changed get_message func to send_message.

--- vk_bot.py
+++ vk_bot.py
@@ -15,7 +15,7 @@ load_dotenv()
 logger = logging.getLogger('dialogflow_bot_logger')
 
 
-def get_message(vk_api, project_id, text, user_id):
+def send_message(vk_api, project_id, text, user_id):
     chat_id = f'vk_{user_id}'
     intent = detect_intent(project_id, chat_id, text)
     if not intent.query_result.intent.is_fallback:
@@ -46,6 +46,6 @@ if __name__ == "__main__":
             text = event.text
             user_id = event.user_id
             try:
-                get_message(vk_api, project_id, text, user_id)
+                send_message(vk_api, project_id, text, user_id)
             except Exception as message:
                 logger.debug(message)
